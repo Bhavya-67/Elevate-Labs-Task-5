bdf7f652e8a46d0483d325851d73bbc040c37c1f
Bhavya-67 <cherukuribhavya216@gmail.com>
2026-02-23 08:06:47 +0530

    Rename file DATA ANALYSIS ON CSV FILE to cvs_data_analysis.py

---
+++ cvs_data_analysis.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Sample data analysis code using pandas
+
+# Load data
+# df = pd.read_csv('data.csv')
+# Perform analysis
+# df.describe()  # Example analysis
+
+# Visualization
+# plt.plot(df['column_name'])
+# plt.show()  # Show plot
